f5171ea613c2a9c301d0184e12a8ce6d774d721e
JhoelHairton <jhoelhairton770@gmail.com>
2024-04-08 00:53:39 -0500

    log

--- ms-catalogo/src/main/java/com/example/mscatalogo/entity/Producto.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/entity/Producto.java
@@ -1,7 +1,9 @@
 package com.example.mscatalogo.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class Producto {
     @Id
--- ms-pedido/src/main/java/com/example/mspedido/repository/PedidosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mspedido.repository;
-
-import com.example.mspedido.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidosRepository extends JpaRepository <Pedido, Integer> {
-}
